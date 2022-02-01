6c9e114c8913122410534052002b88febc62ddd4
BuildTools <hscorp99906@gmail.com>
2022-02-01 03:27:35 -0500

    Modified my bootloader and added paging.

--- Makefile
+++ Makefile
@@ -1,25 +1,21 @@
-default:
-	mkdir -p obj
-	mkdir -p objres
-	mkdir -p bin
-	nasm -fbin src/x86_64/boot/stage1.asm -o bin/bootloader1.bin
-	nasm -felf32 src/x86_64/boot/stage2.asm -o objres/bootloader2.o
-	gcc -c -m64 src/x86_64/kernel/kmain.c -ffreestanding -fno-pie -fstack-protector -mgeneral-regs-only -o obj/kmain.o
-	ld -melf_x86_64 obj/*.o --oformat binary -o bin/kernel.bin
-	ld -melf_i386 -e _stage2 -Ttext 0x1000 objres/*.o --oformat binary -o bin/stage2.bin
-	cat bin/bootloader1.bin bin/stage2.bin bin/kernel.bin > bin/NekoOS.bin
+all:	
+	nasm -fbin src/x86_64/boot/bootloader.asm -o bin/bootloader.bin
+	nasm -felf src/x86_64/kernel/kernel.asm -o objres/kasm.o
+	gcc -c -m32 src/x86_64/kernel/kmain.c -ffreestanding -fno-pie -fstack-protector -mgeneral-regs-only -o obj/kmain.o
+	gcc -c -m32 src/x86_64/kernel/memory/impl/paging.c -ffreestanding -fno-pie -fstack-protector -mgeneral-regs-only -o obj/paging.o
+	ld -melf_i386 -Tlink.ld objres/*.o obj/*.o --oformat binary -o bin/kernel.bin
+	cat bin/bootloader.bin bin/kernel.bin > bin/NekoOS.bin
+	@ # Prepare the image.
 	sudo dd if=/dev/zero of=NekoOS.img bs=1024 count=1440
 	@ # Put the OS stuff in the image.
 	sudo dd if=bin/NekoOS.bin of=NekoOS.img
 
-all:
-	make; make burnusb; make runusb
-
-burnusb:
+burn_usb:
 	sudo dd if=NekoOS.img of=/dev/sdb
 
-runusb:
-	sudo qemu-system-x86_64 -hda /dev/sdb -monitor stdio -d int -no-reboot -D logfile.txt -M smm=off # -soundhw pcspk
+danger:
+	make; make burn_usb run
+
 
 run:
-	sudo qemu-system-x86_64 -fda NekoOS.img -monitor stdio
+	sudo qemu-system-x86_64 -hda /dev/sdb -monitor stdio -d int -no-reboot -D logfile.txt -M smm=off
--- src/x86_64/kernel/kernel.asm
+++ src/x86_64/kernel/kernel.asm
@@ -1,72 +1,16 @@
-bits 16
-global _stage2
+bits 32
+global _kentry
+extern kpge_dir
+extern init_paging
 
-_stage2:
-    ; EDI => dest.
-    mov edi, _l4_page_tbl
-    push edi
-    xor eax, eax
-    mov ecx, 1024
-    rep stosd       ; (*EDI++ = EAX) REP = repeat
-
-    mov edi, _l3_page_tbl
-    mov ecx, 1024
-    rep stosd
-
-    pop edi
-    mov cr3, edi                ; Tells CPU where page table will be.
-    mov eax, _l3_page_tbl
-    add eax, 3
-    mov [edi], eax
-
-    xor eax, eax
-    mov eax, 3 | (1 << 7) | (1 << 2)       ; Setting entry in l3 page table.
-    mov [_l3_page_tbl], eax
-
-    mov ebx, _l4_page_tbl
-    mov ecx, _l3_page_tbl
-
-    ; Enable PA.
-    mov eax, cr4
-    or eax, 1 << 5
-    mov cr4, eax
-
-    ; Set long mode bit.
-    mov ecx, 0xC0000080
-    rdmsr
-    or eax, 1 << 8
-    wrmsr
-
-    mov si, paging_msg
-    call puts
-
-    mov si, page_tbl
-    call puts
-
-    ; Tell CPU we are using paging.
+_kentry:
+    call init_paging
+    mov eax, kpge_dir
+    mov cr3, eax  
     mov eax, cr0
-    or eax, (1 << 31) | 0x1
+    or eax, 0x80000000
     mov cr0, eax
-
-    cli
-
-    lgdt [Pointer]
-    jmp GDT64.Code:LM_START
-
-puts:
-    mov ah, 0xE
-    lodsb
-    or al, al
-    jz rm_ret
-    int 0x10
-    jmp puts
-
-rm_ret: ret
-
-
-paging_msg: db "__PAGING_INITIALIZED__", 0xD, 0xA, 0
-page_tbl: db "0000000000000000-0000000040000000 0000000040000000 -rw", 0xD, 0xA, 0
-
+    hlt
 
 GDT64:                           ; Global Descriptor Table (64-bit).
     .Null: equ $ - GDT64         ; The null descriptor.
@@ -124,7 +68,3 @@ LM_START:
 
 times 2048 db 0x0
 
-align 4096
-section .bss
-_l4_page_tbl: resb 4096
-_l3_page_tbl: resb 4096
